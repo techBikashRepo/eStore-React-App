e4096d694f36d9eac7f315849ebd6e10357d5cc6
Bikash Shaw <1985.bikash.shaw@gmail.com>
2023-06-10 01:00:52 +0530

    TopNav, CatNav, Scss implemented

--- src/App.js
+++ src/App.js
@@ -1,54 +1,12 @@
 import "./App.css";
+import CatNav from "./Components/CatNav";
+import TopNav from "./Components/TopNav";
 
 function App() {
   return (
     <div className="App">
-      <div className="header bg-dark">
-        <div className="row">
-          <div className="brand my-1">
-            <h1>eStore</h1>
-          </div>
-          <div className="inp-container p-0 my-4 w-50 h-25 bg-white">
-            <div className="dropdown m-0 p-0">
-              <select className="select-btn w-100 p-0 m-0">
-                <option value="" key="">
-                  Men
-                </option>
-                <option value="" key="">
-                  Women
-                </option>
-                <option value="" key="">
-                  Kids
-                </option>
-              </select>
-            </div>
-            <input className="form-control" placeholder="Search..." />
-            <button>
-              <i className="fa fa-search" />
-            </button>
-          </div>
-          <div className="login-container p-0">
-            <i className="fa fa-user-circle user-icon" />
-            <h5>
-              <a href="/#">Login</a>
-            </h5>{" "}
-            /{" "}
-            <h5>
-              <a href="/#">Register</a>
-            </h5>
-          </div>
-          <div className="cart-wishlist">
-            <ul className="p-0">
-              <li className="list-icon">
-                <i className="fa fa-heart"></i>
-              </li>
-              <li className="list-icon">
-                <i className="fa fa-shopping-cart"></i>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <TopNav />
+      <CatNav />
     </div>
   );
 }
