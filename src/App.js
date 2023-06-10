3c303b38fb85ac584012621ecda58f70d0e731c0
Bikash Shaw <1985.bikash.shaw@gmail.com>
2023-06-10 13:00:42 +0530

    Product COmponent completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import CatNav from "./Components/CatNav";
 import TopNav from "./Components/TopNav";
+import MainComponent from "./Components/MainComponent";
 
 function App() {
   return (
     <div className="App">
       <TopNav />
       <CatNav />
+      <MainComponent />
     </div>
   );
 }
--- src/Components/TopNav/index.js
+++ src/Components/TopNav/index.js
@@ -5,7 +5,7 @@ const TopNav = () => {
   return (
     <div>
       <div className="header bg-dark">
-        <div className="row">
+        <div className="top-nav-row">
           <div className="brand my-1">
             <h1>eStore</h1>
           </div>
